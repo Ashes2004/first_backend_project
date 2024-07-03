a8403b170d951b7ba1f5ea6c1b07b9ebfe0c72c8
Ashes Das <127014640+Ashes2004@users.noreply.github.com>
2024-07-03 11:07:06 +0530

    modified cors

--- index.js
+++ index.js
@@ -9,40 +9,45 @@ import cors from 'cors';
 import User from "./model/user.model.js";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     session({
-           secret: "ashes das",
-           saveUninitialized:false,
-           resave:false,
-           cookie:{
-            maxAge:60000 * 60 ,
+        secret: "ashes das",
+        saveUninitialized: false,
+        resave: false,
+        cookie: {
+            maxAge: 60000 * 60,
+            httpOnly: true,
+            secure: false
 
-           } ,
-        
-}));
+        },
+
+    }));
 
 const port = process.env.PORT || 3001;
 
-app.listen( port , ()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("Database connected");
-}).catch((err)=> console.log(err));
-app.get("/" , (req,res)=>{
+}).catch((err) => console.log(err));
+app.get("/", (req, res) => {
     console.log(req.session);
     console.log(req.session.id);
     req.session.visited = true;
-    res.cookie('name' , 'ashes' , {maxAge: 60000 * 60 * 2});
-     res.status(201).send("Welcome to MERN backend");
+    res.cookie('name', 'ashes', { maxAge: 60000 * 60 * 2 });
+    res.status(201).send("Welcome to MERN backend");
 });
-app.use("/user" , useRoute);
-app.use("/product" , useProduct);
+app.use("/user", useRoute);
+app.use("/product", useProduct);
 
-app.get('/user/status' , (req , res)=>{
-  return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({msg: "NOt Authenticated"});
+app.get('/user/status', (req, res) => {
+    return req.session.user ? res.status(200).send(req.session.user) : res.status(401).send({ msg: "NOt Authenticated" });
 })
